fc1b3b8288490509efff9895ecb97894bb6f814b
Vikramathithan001 <vikramathithan2002@gmail.com>
2024-05-25 14:05:36 +0530

    landing page create and import logo main from assets

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom'
 const HomeLayout = () => {
   return (
     <div>
-      <nav>Navbar</nav>
       <Outlet />
     </div>
   )
 }
 export default HomeLayout
+ 
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,16 +1,36 @@
 import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main.svg'
+import logo from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom'
 
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background: red;
-  color: white;
-`
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-      <StyledBtn>Click Me</StyledBtn>
-    </div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
+            molestiae unde aliquam neque nihil illum sed quasi deleniti quisquam
+            maiores, itaque sint cumque fuga dignissimos, similique quod,
+            voluptates autem commodi.
+          </p>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
   )
 }
 export default Landing
