5f5541d8c0336809e46ce91e6bb301751708cd46
Vikramathithan001 <vikramathithan2002@gmail.com>
2024-05-25 15:45:53 +0530

    landing page completed

--- client/src/assets/wrappers/LandingPage.js
+++ client/src/assets/wrappers/LandingPage.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Wrapper = styled.section`
   nav {
@@ -11,40 +11,39 @@ const Wrapper = styled.section`
   }
   .page {
     min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
+    display:grid;
+    align-items:center;
   }
-  h1 {
-    font-weight: 700;
-    span {
+  h1{
+    font-weight:700;
+    span{
       color: var(--primary-500);
     }
-    margin-bottom: 1.5rem;
+    margin-bottom:1.5rem;
   }
-  p {
-    line-height: 2;
-    color: var(--text-secondary-color);
-    margin-bottom: 1.5rem;
-    max-width: 35em;
+  p{
+    line-height:2;
+    color:var(--text-secondary-color);
+    margin-bottom:1.5rem;
   }
-  .register-link {
-    margin-right: 1rem;
-  }
+.register-link{
+  margin-right:1rem;
+}
+.btn{
+  padding:.75rem 1rem;
+}
   .main-img {
     display: none;
   }
-  .btn {
-    padding: 0.75rem 1rem;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 400px;
-      column-gap: 3rem;
+  @media (min-width:992px){
+    .page{
+      grid-template-columns:1fr 400px;
+      column-gap:3rem;
     }
-    .main-img {
-      display: block;
+    .main-img{
+      display:block;
+      
     }
   }
-`;
-export default Wrapper;
+`
+export default Wrapper
