3143e1ff3777bc4873c285574f801721643a9794
Vikramathithan001 <vikramathithan2002@gmail.com>
2024-05-25 18:56:04 +0530

    register and login pages comnpleted

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,30 @@
+import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { FormRow, Logo } from '../components'
+
 const Login = () => {
-  return <h1>Login</h1>
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h4>Login</h4>
+        <Logo />
+        <FormRow type='email' name='email' defaultValue='jhon@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='1111' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          explore the app
+        </button>
+        <p>
+          Not a member?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  )
 }
+
 export default Login
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,35 @@
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { FormRow,Logo } from '../components'
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to='/login'>Login page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+
+        <FormRow type='text' name='name' defaultValue='join' />
+        <FormRow
+          type='text'
+          name='lastName'
+          labelText='last name'
+          defaultValue='smith'
+        />
+        <FormRow type='text' name='location' defaultValue='Earth' />
+        <FormRow type='email' name='email' defaultValue='join@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='1009939' />
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 export default Register
