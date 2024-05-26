49eb9533a1a41f99d1decec513f33f1fe90032e0
Vikramathithan001 <vikramathithan2002@gmail.com>
2024-05-26 20:07:54 +0530

    created dashboard all link add elements completed

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,9 +1,23 @@
-import Wrapper from "../assets/wrappers/BigSidebar"
-
+import Wrapper from '../assets/wrappers/BigSidebar'
+import NavLinks from './NavLinks'
+import Logo from './Logo'
+import { useDashboardContext } from '../pages/DashboardLayout'
 const BigSidebar = () => {
+  const { showSidebar } = useDashboardContext()
   return (
     <Wrapper>
-     Bigsidebar
+      <div
+        className={
+          showSidebar ? 'sidebar-container ' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
     </Wrapper>
   )
 }
