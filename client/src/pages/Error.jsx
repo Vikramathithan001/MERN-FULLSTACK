4ac14d3c81e4c7074522e1f70fd75d418a586572
Vikramathithan001 <vikramathithan2002@gmail.com>
2024-05-28 11:52:02 +0530

    create a server and CURD function and linkk mongoDB

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -5,7 +5,7 @@ const Error = () => {
   const error = useRouteError();
   if(error.status ===404){
     return <Wrapper>
-      <div>
+      <div> 
         <img src={img} alt="not found" />
         <h3>Ohh! page not found</h3>
         <p>we can't seem to find the page you are looking for</p>
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+import 'express-async-errors'
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 const app = express()
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+// routers
+import jobRouter from './routes/jobRouter.js'
+//middleware
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js'
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
@@ -11,6 +23,22 @@ app.post('/', (req, res) => {
 
   res.json({ message: 'Data received', data: req.body })
 })
-app.listen(5100,()=>{
- console.log('Server is running on port 5100')
+app.use('/api/v1/jobs', jobRouter)
+
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' })
 })
+
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5100
+
+try {
+  await mongoose.connect(process.env.MONGO_URL)
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+  })
+} catch (error) {
+  console.log(error)
+  process.exit(1)
+}
