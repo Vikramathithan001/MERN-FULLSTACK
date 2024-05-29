27f0cfb054e1d384b3eff26edc7ee703d2b67a1a
Vikramathithan001 <vikramathithan2002@gmail.com>
2024-05-29 16:02:43 +0530

    auth controller all completed and user and login,logout validate the user's

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ import User from '../models/UserModel.js'
 import { comparePassword, hashPassword } from '../utils/passwordUtils.js'
 import { UnauthenticatedError } from '../errors/customErrors.js'
 import { createJWT } from '../utils/tokensUtils.js'
+
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0
   req.body.role = isFirstAccount ? 'admin' : 'user'
@@ -13,6 +14,7 @@ export const register = async (req, res) => {
   const user = await User.create(req.body)
   res.status(StatusCodes.CREATED).json({ msg: 'created user' })
 }
+
 export const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email })
   const isValidUser =
@@ -20,5 +22,19 @@ export const login = async (req, res) => {
 
   if (!isValidUser) throw new UnauthenticatedError('invalid  credentials')
   const token = createJWT({ userId: user._id, role: user.role })
-  res.json({token})
+  const oneDay = 1000 * 60 * 60 * 24
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  })
+  res.status(StatusCodes.OK).json({ msg: 'user Logged in' })
+}
+
+export const logout = (req, res) => {
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  })
+  res.status(StatusCodes.OK).json({ msg: 'user logged out!' })
 }
